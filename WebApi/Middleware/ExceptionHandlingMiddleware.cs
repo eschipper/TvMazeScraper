a003e6c152b1ed028918bb98fe7a7ebbaddd4ce7
Eric Schipper <eric.schipper@devoteam.com>
2023-12-05 09:59:45 +0100

    Adds exception handling middleware

---
+++ WebApi/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json;
+
+namespace WebApi.Middleware
+{
+    public class ExceptionHandlingMiddleware : IMiddleware
+    {
+        private readonly ILogger _logger;
+
+        public ExceptionHandlingMiddleware(ILogger<ExceptionHandlerMiddleware> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex) 
+            {
+                _logger.LogError(ex, "Exception occured in API, message: {Message}", ex.Message);                
+
+                ProblemDetails problemDetails = new()
+                {
+                    Detail = "The server encountered a problem, please try again later",
+                    Status = (int)HttpStatusCode.InternalServerError,
+                    Title = "Internal Server Error"
+                };
+
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsync(JsonSerializer.Serialize(problemDetails));
+            }
+        }
+    }
+}
